7335e536f3b49b7e4188454b6cfe51a4d8b702f4
Alex Joshua Chirwa <153588378+AlexChirwa@users.noreply.github.com>
2024-06-07 13:14:09 +0800

    Add files via upload

---
+++ src/application/Utils.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
+import javafx.scene.image.Image;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import java.io.ByteArrayInputStream;
+
+public class Utils {
+
+    public static Image mat2Image(Mat frame) {
+        try {
+            MatOfByte buffer = new MatOfByte();
+            Imgcodecs.imencode(".png", frame, buffer);
+            return new Image(new ByteArrayInputStream(buffer.toArray()));
+        } catch (Exception e) {
+            System.err.println("Cannot convert Mat object: " + e);
+            return null;
+        }
+    }
+
+    public static <T> void onFXThread(final ObjectProperty<T> property, final T value) {
+        Platform.runLater(() -> {
+            property.set(value);
+        });
+    }
+}
